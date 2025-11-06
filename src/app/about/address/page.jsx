10f946a309800b84a26bdc6e7a4b9340bd9d05bc
MasadRayan <masadrayan2002@gmail.com>
2025-11-06 23:47:00 +0600

    Conditional routing

--- src/app/about/address/page.jsx
+++ src/app/about/address/page.jsx
@@ -1,9 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const AddressPage = () => {
     return (
         <div>
             <h2 className='text-3xl font-bold'>This is the address Page</h2>
+            <Link href={'/about/address/navigate'}>
+                <nav className='hover:text-amber-400 cursor-pointer '>
+                    Navigate
+                </nav>
+            </Link>
         </div>
     );
 };
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,30 @@
+"use client"
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const AboutPage = () => {
+    const route = useRouter();
+    const isLoggedIn = false;
+    const handleNavigate= () =>{
+        if (isLoggedIn) {
+            route.push('/about/address');
+        }
+        else {
+            route.push('/');
+        }
+    }
     return (
         <div>
             <p className='text-3xl font-bold'>This is the about page</p>
+            <p>
+                <Link href={'/about/address'}>
+                    <button className='hover:text-amber-500 cursor-pointer'>Address</button>
+                </Link>
+            </p>
+            <button className='btn hover:text-amber-400' type='button' onClick={handleNavigate}>
+                Address Page
+            </button>
         </div>
     );
 };
