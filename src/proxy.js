1d282f18b74511dd76563d6e30dc655191ccec8a
MasadRayan <masadrayan2002@gmail.com>
2025-11-07 21:14:53 +0600

    Proxy added

---
+++ src/proxy.js
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function proxy(request) {
+
+    const userInfo = {
+        role: 'user',
+        email: 'test@admin.com',
+    }
+
+    let isServices = request.nextUrl.pathname.startsWith('/services');
+    let isAdmin = userInfo.role == 'admin';
+
+    if (isServices && !isAdmin) {
+        return NextResponse.rewrite(new URL('/not-found', request.url));
+        
+    }
+    return NextResponse.next()
+}
+
