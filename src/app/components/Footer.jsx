491145c18acef6669304e5e121f20bf4e5f6ede2
MasadRayan <masadrayan2002@gmail.com>
2025-11-08 14:00:34 +0600

    Footer style changed

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='text-center bg-slate-600 '>
+        <div className='text-center bg-black text-white py-3 '>
             This is a next js app
         </div>
     );
