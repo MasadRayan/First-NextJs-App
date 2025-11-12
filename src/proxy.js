261d1ca1b548c00c2b02b3619d4b5e6edd6389c9
MasadRayan <masadrayan2002@gmail.com>
2025-11-12 13:33:22 +0600

    User redirect done

--- src/proxy.js
+++ src/proxy.js
@@ -1,20 +1,19 @@
+import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server'
+import { boolean } from 'zod';
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request) {
+export async function proxy(req) {
 
-    const userInfo = {
-        role: 'user',
-        email: 'test@admin.com',
+    const token = await getToken({ req });
+    const isToken = Boolean(token);
+    const isAdminUser = token?.role == 'admin';
+    const isAdminSpecificPage = req.nextUrl.pathname.startsWith('/services');
+    if (isAdminSpecificPage && ! isAdminUser) {
+        const callBackUrl = encodeURIComponent(req.nextUrl.pathname);
+        return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${callBackUrl}`, req.url));
     }
 
-    let isServices = request.nextUrl.pathname.startsWith('/hi');
-    let isAdmin = userInfo.role == 'admin';
-
-    if (isServices && !isAdmin) {
-        return NextResponse.rewrite(new URL('/not-found', request.url));
-        
-    }
-    return NextResponse.next()
+    return NextResponse.next();
 }
 
